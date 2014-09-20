b94f1a991551e04025d065aafbfa62f800f9deb9
= <=>
2014-09-20 19:38:24 +0800

    hide pwd

--- conn.php
+++ conn.php
@@ -4,7 +4,7 @@
  * Author   ：李卓立，高奕丽，韩畅，韦权
  * 
 */
-$conn = mysql_connect("localhost","lzl","941126");
+$conn = mysql_connect("localhost","*","*");
 if(!$conn)
 {
 	echo "<script>alert(' 对不起，服务器出了点问题，请稍等或联系我们');window.location.href='index.html';</script>";
